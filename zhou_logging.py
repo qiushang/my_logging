bd63779c19478023374bf029244bc4a6c7f08c60
周同学 <zhouqiushang@126.com>
2019-01-03 16:22:52 +0800

    Update zhou_logging.py

--- zhou_logging.py
+++ zhou_logging.py
@@ -6,11 +6,10 @@ import time
 # debug level logger
 my_logger_debug = logging.getLogger("debug_logger")
 my_logger_debug.setLevel(logging.DEBUG)
+"""
+formatter的参数设置
+"""
 formatter = logging.Formatter('%(name)-12s %(asctime)s level-%(levelname)-8s thread-%(thread)-8d %(message)s')   # 每行日志的前缀设置
-debug_handler = TimedRotatingFileHandler("logging_debug", "midnight", 1)
-debug_handler.suffix = "%Y-%m-%d_%H-%M-%S.log"
-debug_handler.setFormatter(formatter)
-my_logger_debug.addHandler(debug_handler)
 """
 when 是一个字符串的定义如下：
 “S”: Seconds
@@ -20,6 +19,11 @@ when 是一个字符串的定义如下：
 “W”: Week day (0=Monday)
 “midnight”: Roll over at midnight
 """
+debug_handler = TimedRotatingFileHandler("logging_debug", "midnight", 1)
+debug_handler.suffix = "%Y-%m-%d_%H-%M-%S.log"
+debug_handler.setFormatter(formatter)
+my_logger_debug.addHandler(debug_handler)
+
 # info level logger
 my_logger_info = logging.getLogger("info_logger")
 my_logger_info.setLevel(logging.INFO)
